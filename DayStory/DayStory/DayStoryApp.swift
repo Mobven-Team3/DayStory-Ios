044f9f83235f204c5eeeeef02faecfa08c1800f3
Recep Taha AYDIN <115106072+receptahaydin@users.noreply.github.com>
2024-05-17 11:08:03 +0300

    Created project

---
+++ DayStory/DayStory/DayStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayStoryApp.swift
+//  DayStory
+//
+//  Created by Recep Taha Aydın on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct DayStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
