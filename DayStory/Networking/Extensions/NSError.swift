f6810f3da58ec90a8dcdfc24bd89290e6ae16afb
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-20 13:39:55 +0300

    Generic error class updated and Networkable extensions created

--- DayStory/Networking/Extensions/NSError.swift
+++ DayStory/Networking/Extensions/NSError.swift
@@ -11,4 +11,8 @@ public extension NSError {
     static var generic: Error {
         NSError(domain: "", code: -1, userInfo: [:])
     }
+    
+    static func genericError(message: String) -> NSError {
+        NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey : message])
+    }
 }
