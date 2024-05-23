eb2033ac6355d89ab8a8e59e36914ec37b4399be
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-23 21:53:41 +0300

    Removed file that shouldn't be tracked

---
+++ DayStory/DayStory/Views/Buttons/GradientButton.swift
@@ -0,0 +1,18 @@
+//
+//  GradientButton.swift
+//  DayStory
+//
+//  Created by Recep Taha Aydın on 23.05.2024.
+//
+
+import SwiftUI
+
+struct GradientButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientButton()
+}
