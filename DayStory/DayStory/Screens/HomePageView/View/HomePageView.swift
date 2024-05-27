5e004bb57454139fffc0287a3d0b5b9a4f7db7b8
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-27 15:26:40 +0300

    Comflict solved

---
+++ DayStory/DayStory/Screens/HomePageView/View/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  DayStory
+//
+//  Created by Recep Taha Aydın on 27.05.2024.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomePageView()
+}
