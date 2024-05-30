9f1e1af5c6d0c88ecc02efd6e3e45f191b3fe9c7
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-30 20:37:44 +0300

    SignupPersonalViewModel updated
    Regex control moved to String+Extensions

--- DayStory/DayStory/Screens/SignupPersonalView/ViewModel/SignupPersonalViewModel.swift
+++ DayStory/DayStory/Screens/SignupPersonalView/ViewModel/SignupPersonalViewModel.swift
@@ -19,17 +19,9 @@ final class SignupPersonalViewModel: ObservableObject {
     @Published var lastNameErrorMessage: String? = nil
     @Published var genderErrorMessage: String? = nil
     
-    
     func validateFields() {
-        let nameRegex = "^[a-zA-Z]+$"
-        let nameTest = NSPredicate(format:"SELF MATCHES %@", nameRegex)
-        
-        nameErrorMessage = name.isEmpty ? "İsim boş olamaz." : nil
-        nameErrorMessage = !nameTest.evaluate(with: name) ? "İsimde geçersiz karakterler var." : nil
-        
-        lastNameErrorMessage = lastName.isEmpty ? "Soyisim boş olamaz." : nil
-        lastNameErrorMessage = !nameTest.evaluate(with: lastName) ? "Soyisimde geçersiz karakterler var." : nil
-        
+        nameErrorMessage = !name.isValidName ? "Geçerli bir isim giriniz." : nil
+        lastNameErrorMessage = !lastName.isValidName ? "Geçerli bir soyisim giriniz." : nil
         genderErrorMessage = selectedGender == "" ? "Lütfen bir cinsiyet seçiniz." : nil
         
         isValid = nameErrorMessage == nil && lastNameErrorMessage == nil && genderErrorMessage == nil
