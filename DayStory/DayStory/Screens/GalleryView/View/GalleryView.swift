c74aa5e92fa4196efab952bb0fbb710bd72897c5
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-31 23:55:48 +0300

    TabView and three tab item view created

---
+++ DayStory/DayStory/Screens/GalleryView/View/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  DayStory
+//
+//  Created by Recep Taha Aydın on 31.05.2024.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery View")
+    }
+}
+
+#Preview {
+    GalleryView()
+}
