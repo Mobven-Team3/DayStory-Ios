14fa0a86fae8582a80ae3e0aa9810b9daeb1d4b0
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-04 18:57:27 +0300

    Email and name validations updated

--- DayStory/DayStory/Extensions/String+Extensions.swift
+++ DayStory/DayStory/Extensions/String+Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension String {
     var isValidEmail: Bool {
-        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
+        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{3,}").evaluate(with: self)
     }
     
     var isValidPassword: Bool {
@@ -17,7 +17,7 @@ extension String {
     }
     
     var isValidName: Bool {
-        NSPredicate(format: "SELF MATCHES %@", "^[a-zA-ZçÇğĞıİöÖşŞüÜ]{3,}$").evaluate(with: self)
+        NSPredicate(format: "SELF MATCHES %@", "^[a-zA-ZçÇğĞıİöÖşŞüÜ ]{2,}$").evaluate(with: self)
     }
     
     var isValidUserName: Bool {
