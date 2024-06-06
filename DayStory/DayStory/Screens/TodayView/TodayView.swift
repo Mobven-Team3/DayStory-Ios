1f7e8cf12105f0be67a55ec2bedcd8a204d4d3c6
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-06 16:16:57 +0300

    Date extension used on TodayView, for the note count, screen design updated
    If there are no notes created, it says "You have no notes" on the screen. If there are, the notes are listed. CreateNoteView navigation title updated as today's date.

--- DayStory/DayStory/Screens/TodayView/TodayView.swift
+++ DayStory/DayStory/Screens/TodayView/TodayView.swift
@@ -8,26 +8,46 @@
 import SwiftUI
 
 struct TodayView: View {
+    
+    let date = Date()
+    var noteCount = 5
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack {
-                    Text("gg.aa.yyyy")
+                    Text(date.toString(format: "dd.MM.yyyy"))
                         .font(.callout)
                         .padding(.vertical, 35)
                     
-                    GradientButton(title: "AI Gün Özeti Oluştur")
-                        .opacity(0.5)
+                    Button {
+                        
+                    } label: {
+                        GradientButton(title: "AI Gün Özeti Oluştur")
+                    }
+                    .disabled(noteCount == 0)
+                    .opacity(noteCount == 0 ? 0.5 : 1)
                     
                     Text("Notlar")
                         .font(.callout)
                         .fontWeight(.semibold)
                         .padding(.top, 45)
+                        .padding(.bottom, 20)
                     
-                    Text("Bu gün için notunuz bulunmuyor.")
-                        .font(.callout)
-                        .foregroundStyle(.todayScreenText)
-                        .padding()
+                    if noteCount == 0 {
+                        Text("Bu gün için notunuz bulunmuyor.")
+                                                .font(.callout)
+                                                .foregroundStyle(.todayScreenText)
+                                                .padding()
+                    } else {
+                        VStack(spacing: 25) {
+                            ForEach(0..<noteCount) {_ in
+                                NoteListingView()
+                            }
+                        }
+                    }
+                    
+                    Spacer(minLength: 80)
                 }
             }
             .overlay(alignment: .bottomTrailing) {
@@ -45,6 +65,7 @@ struct TodayView: View {
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(date.toString(format: "dd.MM.yyyy"))
             .toolbar {
                 DayStoryToolbar()
             }
