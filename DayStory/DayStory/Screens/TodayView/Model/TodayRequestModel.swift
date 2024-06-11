a73cb494b0faa07bea992bfb79424bd5cfe052c3
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-12 00:10:50 +0300

    AI image generation request and response models created

---
+++ DayStory/DayStory/Screens/TodayView/Model/TodayRequestModel.swift
@@ -0,0 +1,12 @@
+//
+//  TodayRequestModel.swift
+//  DayStory
+//
+//  Created by Recep Taha Aydın on 11.06.2024.
+//
+
+import Foundation
+
+public struct CreateDaySummaryContract: Encodable {
+    let date: String
+}
